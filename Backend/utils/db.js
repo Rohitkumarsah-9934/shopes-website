2f5fe3de02b306702d314aa93e5b0ff167d3cece
Rohit Kumar Sah <rohitkumar912804@gmail.com>
2026-04-15 14:03:04 +0530

    first comment

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,14 +1,13 @@
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB Connected Successfully");
-    } catch (error) {
-        console.log("Mongodb Failed:", error.message);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(` MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(` MongoDB Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
