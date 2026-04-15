58432fbe8c1bc9dac4b5140a844b494798b23365
Rohit Kumar Sah <rohitkumar912804@gmail.com>
2026-04-15 14:30:35 +0530

    first commit

--- Backend/Router/admin-router.js
+++ Backend/Router/admin-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const adminController = require("../controllers/admin-controller");
+const adminController = require("../Controllers/admin-controller");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const adminMiddleware = require("../middlewares/admin-middleware");
--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./utils/db");
-const authRoute = require("./router/auth-router");
-const contactRoute = require("./router/contact-router");
-const serviceRoute = require("./router/service-router");
-const adminRoute = require("./router/admin-router");
+const authRoute = require("./Router/auth-router");
+const contactRoute = require("./Router/contact-router");
+const serviceRoute = require("./Router/service-router");
+const adminRoute = require("./Router/admin-router");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const app = express();
